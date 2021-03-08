90852f2a176ed32696fd4e4444c6ed4fba2b2f11
jjbc01 <jordi2323@hotmail.com>
2021-03-08 13:13:39 +0100

    Terminadas las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TemarioComponent } from './pages/temario/temario.component';
+import { DirectivasComponent } from './pages/directivas/directivas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'temario', component: TemarioComponent},
+  {path:'directivas', component: DirectivasComponent},
+  {path:'**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
