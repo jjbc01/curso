ad2078cdd83c15a5656cf2a478dbce073ce48494
jjbc01 <jordi2323@hotmail.com>
2021-03-08 18:06:48 +0100

    Cambios para mejora

--- src/app/pages/directivas/directivas.component.ts
+++ src/app/pages/directivas/directivas.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivasComponent implements OnInit {
 
+  titulo = 'Centros educativos';
+
+  colegio = 'Colegio Madre de la Vida';
+
+  mostrarColegio = true;
+
+  datos = [1,2,4,5,6];
+
+  colegios = ['Nara', 'Greco', 'Mayol', 'Sefarad'];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  mostrarOcultar(): void {
+
+    this.mostrarColegio = !this.mostrarColegio;
   }
 
 }
